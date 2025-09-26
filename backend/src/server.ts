386dd3b06cb98bdf9a93234a63e7fb3e6cb3b01c
Jeffrey M. Aspiras <jefsohandsome1@gmail.com>
2025-09-26 14:49:43 +0800

    initial commit with prisma

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import dotenv from 'dotenv'
+import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+dotenv.config()
+
+const server = Fastify({
+    logger: true
+}).withTypeProvider<TypeBoxTypeProvider>()
+
+const port: number = Number(process.env.HTTP_PORT)? Number(process.env.HTTP_PORT) : 3000;
+const host: string = process.env.HOST? String(process.env.HOST) : '127.0.0.1';
+
+try{
+    await server.listen({
+        port: port,
+        host: host
+    })
+    server.log.info(`Server is running on port: ${port}`)
+}catch(err: unknown){
+    server.log.info("Server failed to run")
+    server.close();
+}
+    
