e70968febe8bdf9b3464d36fd9342bc676cb4f82
Jeffrey M. Aspiras <jefsohandsome1@gmail.com>
2025-09-30 11:08:47 +0800

    added creation of patient schema

---
+++ backend/src/type-schemas/patient-schemas.ts
@@ -0,0 +1,21 @@
+import { Type, type Static } from "@fastify/type-provider-typebox";
+
+export const createPatientSchema = Type.Object({
+    firstName: Type.String({minLength: 2, maxLength: 30}),
+    lastName: Type.String({minLength: 2, maxLength: 30}),
+    middleName: Type.Union(
+        [
+            Type.String({minLength: 3, maxLength: 30}), 
+            Type.Undefined(),
+            Type.Null()
+        ]
+    ),
+    birthDate: Type.String(),
+    csdIdOrPwdId: Type.String(),
+    mobileNumber: Type.String(),
+    residentialAddress: Type.String(),
+    createdById: Type.String(),
+    updatedById: Type.String()
+});
+
+export type createPatientType = Static<typeof createPatientSchema>;
