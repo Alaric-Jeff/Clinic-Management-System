34fa695eec08243526f2ae88ddde2fa56e03cb25
Jeffrey M. Aspiras <jefsohandsome1@gmail.com>
2025-11-07 12:31:10 +0800

    added more rate-limitting in routes

--- backend/src/routers/search-engine-routes.ts
+++ backend/src/routers/search-engine-routes.ts
@@ -8,71 +8,93 @@ import { searchMedicalServiceController } from "../controllers/search-engine-con
 
 import { searchServiceSchema, searchServiceEngineResponse } from "../type-schemas/search-engine-schemas/search-service-schema.js";
 import { searchBodySchema, searchPatientEngineResponse } from "../type-schemas/search-engine-schemas/search-patient-schema.js";
-import {getTotalPatientsParams, totalPatientPaginatedResponseSchema} from '../type-schemas/patients/get-total-paginated-schema.js'
+import { getTotalPatientsParams, totalPatientPaginatedResponseSchema } from "../type-schemas/patients/get-total-paginated-schema.js";
 import { requireRole } from "../hooks/authorization.js";
 import { Role } from "@prisma/client";
 
-export async function searchEngineRoutes(
-    fastify: FastifyInstance
-){
-    fastify.route({
-        method: "POST",
-        url: '/search-patient',
-        schema: {
-            body: searchBodySchema,
-            response: {
-                200: searchPatientEngineResponse
-            }
-        }, preHandler: requireRole([Role.admin, Role.encoder]),
-        handler: searchPatientController
-    })
+export async function searchEngineRoutes(fastify: FastifyInstance) {
+  fastify.route({
+    method: "POST",
+    url: "/search-patient",
+    config: {
+      rateLimit: {
+        max: 20, 
+        timeWindow: "1 minute", 
+      },
+    },
+    schema: {
+      body: searchBodySchema,
+      response: { 200: searchPatientEngineResponse },
+    },
+    preHandler: requireRole([Role.admin, Role.encoder]),
+    handler: searchPatientController,
+  });
 
-    fastify.route({
-        method: 'GET',
-        url: '/search-week-patients',
-        schema: {
-            querystring: getTotalPatientsParams,
-            response: {
-                200: totalPatientPaginatedResponseSchema
-            }
-        }, preHandler: requireRole([Role.admin, Role.encoder]),
-        handler: searchPatientWeekController
-    })
+  fastify.route({
+    method: "GET",
+    url: "/search-week-patients",
+    config: {
+      rateLimit: {
+        max: 10,
+        timeWindow: "1 minute",
+      },
+    },
+    schema: {
+      querystring: getTotalPatientsParams,
+      response: { 200: totalPatientPaginatedResponseSchema },
+    },
+    preHandler: requireRole([Role.admin, Role.encoder]),
+    handler: searchPatientWeekController,
+  });
 
-    fastify.route({
-        method: 'GET',
-        url: '/search-month-patients',
-        schema: {
-            querystring: getTotalPatientsParams,
-            response: {
-                200: totalPatientPaginatedResponseSchema
-            }
-        }, preHandler: requireRole([Role.admin, Role.encoder]),
-        handler: searchPatientMonthController
-    })
+  fastify.route({
+    method: "GET",
+    url: "/search-month-patients",
+    config: {
+      rateLimit: {
+        max: 10,
+        timeWindow: "1 minute",
+      },
+    },
+    schema: {
+      querystring: getTotalPatientsParams,
+      response: { 200: totalPatientPaginatedResponseSchema },
+    },
+    preHandler: requireRole([Role.admin, Role.encoder]),
+    handler: searchPatientMonthController,
+  });
 
-    fastify.route({
-        method: 'GET',
-        url: '/search-today-patients',
-        schema: {
-            querystring: getTotalPatientsParams,
-            response: {
-                200: totalPatientPaginatedResponseSchema
-            }
-        }, preHandler: requireRole([Role.admin, Role.encoder]),
-        handler: searchPatientDayController
-    })
+  fastify.route({
+    method: "GET",
+    url: "/search-today-patients",
+    config: {
+      rateLimit: {
+        max: 15,
+        timeWindow: "1 minute",
+      },
+    },
+    schema: {
+      querystring: getTotalPatientsParams,
+      response: { 200: totalPatientPaginatedResponseSchema },
+    },
+    preHandler: requireRole([Role.admin, Role.encoder]),
+    handler: searchPatientDayController,
+  });
 
-    fastify.route({
-        method: "POST",
-        url: '/search-medical-service',
-        schema: {
-            body: searchServiceSchema,
-            response: {
-                200: searchServiceEngineResponse
-            }
-        }, preHandler: requireRole([Role.admin, Role.encoder]),
-        handler: searchMedicalServiceController
-    })    
-
+  fastify.route({
+    method: "POST",
+    url: "/search-medical-service",
+    config: {
+      rateLimit: {
+        max: 15,
+        timeWindow: "1 minute",
+      },
+    },
+    schema: {
+      body: searchServiceSchema,
+      response: { 200: searchServiceEngineResponse },
+    },
+    preHandler: requireRole([Role.admin, Role.encoder]),
+    handler: searchMedicalServiceController,
+  });
 }
